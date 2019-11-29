8784236cd3582cc531ef4ee4034e4ecc91c19a5f
Daniel Shi <lystyp87015@gamil.com>
2019-11-29 16:19:02 +0800

    Change MySQL code to SQL server version.

--- models/member/update_model.js
+++ models/member/update_model.js
@@ -1,19 +1,26 @@
 const db = require('../connection_db');
+var TYPES = require('tedious').TYPES;
+var Request = require('tedious').Request;
 
 module.exports = function update(id, memberUpdateData) {
     let result = {};
     return new Promise((resolve, reject) => {
-        db.query('UPDATE member_info SET ? WHERE id = ?', [memberUpdateData, id], function (err, rows) {
-            if (err) {
-                console.log(err);
-                result.status = "會員資料更新失敗。"
-                result.err = "伺服器錯誤，請稍後在試！"
-                reject(result);
-                return;
+        request = new Request('UPDATE member_info SET @data WHERE id = @id',
+            function(err, rowCount, rows) {
+                if (err) {
+                    console.log(err);
+                    result.status = "會員資料更新失敗。"
+                    result.err = "伺服器錯誤，請稍後在試！"
+                    reject(result);
+                    return;
+                }
+                result.status = "會員資料更新成功。"
+                result.memberUpdateData = memberUpdateData
+                resolve(result)
             }
-            result.status = "會員資料更新成功。"
-            result.memberUpdateData = memberUpdateData
-            resolve(result)
-        })
+        );
+        request.addParameter('data', TYPES.NVarChar, memberUpdateData);
+        request.addParameter('id', TYPES.Int, id);
+        db.execSql(request);
     })
 }
